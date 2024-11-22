581ba0fb7857c9e1d4a2cee9360cc12873a5eee6
BobbyDigitz <76012943+BobbyDigitz@users.noreply.github.com>
2024-11-22 15:57:44 -0800

    Update test_powershell.py

--- tests/shells/test_powershell.py
+++ tests/shells/test_powershell.py
@@ -10,4 +10,4 @@ from thefuck.shells import  ['powershell.exe']),
         Popen.return_value.stdout.read.side_effect = RuntimeError
         with pytest.raises(RuntimeError):
             shell._get_version()
-        assert Popen.call_args[0][0] == ['powershell.exe', '$PSVersionTable.PSVersion']
+        assert Popen.call_args[0][0] == ['powershell.exe', '$PSVersionrsion']
