330a141e10db337482e75b49b5f83bb9a1ec021c
BobbyDigitz <76012943+BobbyDigitz@users.noreply.github.com>
2024-11-22 16:02:36 -0800

    Update test_sudo.py

--- tests/specific/test_sudo.py
+++ tests/specific/test_sudo.py
@@ -11,9 +11,4 @@ from thefuck.types import Command
     (True, 'ls', 'ls', True),
     (False, 'sudo ls', 'ls', False),
     (False, 'ls', 'ls', False)])
-def test_sudo_support(return_value, command, called, result):
-    def fn(command):
-        assert command == Command(called, '')
-        return return_value
-
-    assert sudo_support(fn)(Command(command, '')) == result
+l == result
